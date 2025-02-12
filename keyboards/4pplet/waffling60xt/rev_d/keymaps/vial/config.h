983ec71afef19751dac721d5e0f6945c903e8c4e
blonski3 <bln157139@gmail.com>
2025-02-12 21:08:40 +0100

    60xt

---
+++ keyboards/4pplet/waffling60xt/rev_d/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x64, 0xEF, 0x37, 0x98, 0x8B, 0x6B, 0x3D, 0x43}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
